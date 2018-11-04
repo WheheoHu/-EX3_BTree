b546d62a6a897e36c6270d6f358733b445d5f38b
文昊 胡 <hwh19981124@hotmail.com>
2018-11-04 14:45:24 +0800

    new

--- BTree/btree.h
+++ BTree/btree.h
@@ -1,1 +1,18 @@
 #pragma once
+template<typename T>
+struct BtreeNode
+{
+	T data;
+	BtreeNode *lchild.*rchild;
+};
+
+template<class T>
+class BTree
+{
+public:
+	BTree();
+	~BTree();
+	
+private:
+
+};
